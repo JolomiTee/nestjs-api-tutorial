1698ed3e76f32b4a0d134233ee12d8fdc7013968
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-15 04:13:17 +0100

    - CREATED MY FIRST NESTJS API ROUTE

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+	// service is where the business logic is offloaded
+export class AuthService {
+	signup() {
+		return "I wanna sign up"
+	}
+	signin() {
+		return 'I wanna sign in'
+	}
+}
