b3130f8e6ba9dfd45839868454108a6585a22e3a
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-17 02:26:44 +0100

    - implementing basic business logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 @Injectable({})
 	// service is where the business logic is offloaded
 export class AuthService {
 	constructor(private prisma: PrismaService) { }
 
-	signup() {
-		return "I wanna sign up"
+	async signup(dto: AuthDto) {
+		// generate password hash
+		const hash = await argon.hash(dto.password)
+
+		// save new user in the db
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				hash
+			}
+		})
+
+		// return the saved user
+		return user
 	}
 
 	signin() {
