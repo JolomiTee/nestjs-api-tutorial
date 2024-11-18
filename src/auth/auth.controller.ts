877845aa44484b66532fc781ebd092f4db8ed027
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-18 23:05:04 +0100

    - created signin api logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,12 +11,11 @@ export class AuthController {
 	signup(
 		@Body() dto: AuthDto
 	) {
-		console.log({dto})
 		return this.authService.signup(dto)
 	}
 
 	@Post('signin')
-	signin() {
-		return this.authService.signin()
+	signin(@Body() dto: AuthDto) {
+		return this.authService.signin(dto)
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 @Injectable({})
 	// service is where the business logic is offloaded
 export class AuthService {
@@ -10,20 +11,58 @@ export class AuthService {
 	async signup(dto: AuthDto) {
 		// generate password hash
 		const hash = await argon.hash(dto.password)
+		try {
+			// save new user in the db
+			const user = await this.prisma.user.create({
+				data: {
+					email: dto.email,
+					hash
+				},
+				// select: {
+				// 	id: true,
+				// 	email: true,
+				// 	createdAt: true
+				// }
+			})
+			delete user.hash
 
-		// save new user in the db
-		const user = await this.prisma.user.create({
-			data: {
-				email: dto.email,
-				hash
+			// return the saved user
+			return user
+
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException ('Credentials taken', 'User with your credentials already exists, try again with something else')
+				}
+			}
+		}
+
+	}
+
+	async signin(dto: AuthDto) {
+		// find user by mail
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
 			}
 		})
 
-		// return the saved user
-		return user
-	}
+		// if no user throw exception
+		if (!user) {
+			throw new ForbiddenException("Incorrect credentials", 'your details do not seem to be correct, please try again')
+		}
 
-	signin() {
-		return 'I wanna sign in'
+		// compare password
+		const matchedPwd = await argon.verify(user.hash, dto.password)
+
+		// if incorrect, exception
+		if (!matchedPwd) {
+			throw new ForbiddenException("Incorrect credentials", 'your details do not seem to be correct, please try again')
+		}
+
+		delete user.hash
+
+		// all correct? send back user
+		return user
 	}
 }
