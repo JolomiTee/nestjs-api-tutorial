48005138a6e8fd6bc25841873524e64aab9b1f3d
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-18 23:49:24 +0100

    getting started with jwts in nestjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
 @Injectable({})
 	// service is where the business logic is offloaded
 export class AuthService {
-	constructor(private prisma: PrismaService) { }
+	constructor(
+		private prisma: PrismaService,
+		private jwt: JwtService
+	) { }
 
 	async signup(dto: AuthDto) {
 		// generate password hash
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-	constructor() {
+	constructor(config: ConfigService) {
 		super({
 			datasources: {
 				db: {
-					url: "postgresql://postgres:ICELAKE1029@localhost:5434/bird?schema=public"
+					url: config.get('DATABASE_URL')
 				}
 			}
 		})
+		console.log(config.get('DATABASE_URL'))
 	}
 }
